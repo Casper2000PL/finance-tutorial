d11c5d403633ad1b13b73cfd1372defe7b85d828
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-10-05 18:51:11 +0200

    5. Header component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <UserButton />
-      <Button>Click Me</Button>
+      Dashboard Page
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,13 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
