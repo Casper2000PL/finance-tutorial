c3006a104b064ddae2659a9af2b24620af84d344
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-11-07 20:13:08 +0100

    20. Dashboard cards

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,9 @@
-"use client";
+import DataGrid from "@/components/data-grid";
 
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  const { isOpen, onOpen } = useNewAccount();
-
+export default function DashboardPage() {
   return (
-    <div>
-      Home Page
-      <Button onClick={onOpen}>Open</Button>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
 }
---
+++ components/data-grid.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
+import { formatDateRange } from "@/lib/utils";
+import { useSearchParams } from "next/navigation";
+import { FaPiggyBank } from "react-icons/fa";
+import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
+import { DataCard } from "@/components/date-card";
+
+const DataGrid = () => {
+  const { data } = useGetSummary();
+
+  const params = useSearchParams();
+  const to = params.get("to") || undefined;
+  const from = params.get("from") || undefined;
+
+  const dataRangeLabel = formatDateRange({ to, from });
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+      <DataCard
+        title="Remaining"
+        value={data?.remainingAmount}
+        percentageChange={data?.remainingChange}
+        icon={FaPiggyBank}
+        variant="default"
+        dateRange={dataRangeLabel}
+      />
+      <DataCard
+        title="Income"
+        value={data?.incomeAmount}
+        percentageChange={data?.incomeChange}
+        icon={FaArrowTrendUp}
+        variant="success"
+        dateRange={dataRangeLabel}
+      />
+      <DataCard
+        title="Expenses"
+        value={data?.expensesAmount}
+        percentageChange={data?.expensesChange}
+        icon={FaArrowTrendDown}
+        variant="danger"
+        dateRange={dataRangeLabel}
+      />
+    </div>
+  );
+};
+
+export default DataGrid;
